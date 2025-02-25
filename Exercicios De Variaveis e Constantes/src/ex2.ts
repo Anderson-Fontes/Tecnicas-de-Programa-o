7ee5f8975d6e432d92d71b2d6d640fcf5b8d7141
Anderson Fontes Fernandes Júnior <andersonfontes795@gmail.com>
2025-02-24 22:15:52 -0300

    Add files via upload

---
+++ Exercicios De Variaveis e Constantes/src/ex2.ts
@@ -0,0 +1,7 @@
+let aluno1: number = 7.5;
+let aluno2: number = 8;
+let aluno3: number = 9;
+
+let media = (aluno1 + aluno2 + aluno3) / 3;
+
+console.log(media);
---
+++ Exercicios De Variaveis e Constantes/src/ex3.ts
@@ -0,0 +1,12 @@
+let a: number = 10;
+
+let b: number = 20;
+
+let c: number = a;
+
+ a = b;
+
+ b = c;
+
+console.log(a);
+console.log(b);
